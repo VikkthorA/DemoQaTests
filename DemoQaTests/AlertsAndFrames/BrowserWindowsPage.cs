8f075f0df1bc37a3974c0b3f741b79ec94f676c3
VikkthorA <vikiangelov24@gmail.com>
2026-03-17 12:57:42 +0200

    Implement BrowserWindowsPage and AlertsAndFramesTests to verify new tab and window behavior on DemoQA
    - Create BrowserWindowsPage class with methods to open new tab/window and read sample heading
    - Add AlertsAndFramesTests.NewWindows() test to verify new tab and window content

---
+++ DemoQaTests/AlertsAndFrames/BrowserWindowsPage.cs
@@ -0,0 +1,35 @@
+﻿using DemoQaTests.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+public class BrowserWindowsPage : BasePage
+{
+    public BrowserWindowsPage(IWebDriver driver) : base(driver) { }
+
+    private readonly By _newTabButton = By.Id("tabButton");
+    private readonly By _newWindowButton = By.Id("windowButton");
+    private readonly By _newWindowMessageButton = By.Id("messageWindowButton");
+
+    public string GetTextFromNewTab() // works as well for new window, because the content is the same
+    {
+        Driver.FindElement(_newTabButton).Click();
+        Driver.SwitchTo().Window(Driver.WindowHandles.Last());
+        string text = Driver.FindElement(By.Id("sampleHeading")).Text;
+        Driver.Close();
+        Driver.SwitchTo().Window(Driver.WindowHandles.First());
+        return text;
+    }
+
+    public void OpenAndCloseNewWindowMessage()
+    {
+        string originalWindow = Driver.CurrentWindowHandle;
+
+        Driver.FindElement(_newWindowMessageButton).Click();
+
+        var newWindow = Driver.WindowHandles.Last();
+
+        Driver.SwitchTo().Window(newWindow);
+        Driver.Close();
+        Driver.SwitchTo().Window(originalWindow);
+    }
+}
---
+++ DemoQaTests/Tests/AlertsAndFramesTests.cs
@@ -0,0 +1,22 @@
+﻿using DemoQaTests.Base;
+using DemoQaTests.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+
+public class AlertsAndFramesTests : BaseTest
+{
+    [Fact]
+    public void NewWindows()
+    {
+        var browserWindowsPage = new BrowserWindowsPage(Driver);
+        
+        Driver.Navigate().GoToUrl("https://demoqa.com/browser-windows");
+        var newTabText = browserWindowsPage.GetTextFromNewTab();
+        var newWindowText = browserWindowsPage.GetTextFromNewTab();
+        browserWindowsPage.OpenAndCloseNewWindowMessage();
+        Assert.Equal("This is a sample page", newWindowText);
+        Assert.Equal("This is a sample page", newTabText);
+    }
+
+}
