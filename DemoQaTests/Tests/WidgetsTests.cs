757418b1f006ed108ec483a42086d2fdcbbde498
VikkthorA <vikiangelov24@gmail.com>
2026-03-19 14:24:12 +0200

    Added AccordianPage: - Locate accordion buttons using CssSelector stored in IList - Scroll to button with JS scrollIntoView before click - Test iterates all buttons and asserts aria-expanded is true

---
+++ DemoQaTests/Tests/WidgetsTests.cs
@@ -0,0 +1,20 @@
+﻿using DemoQaTests.Base;
+using DemoQaTests.Widgets;
+
+public class WidgetsTests : BaseTest
+{
+    [Fact]
+    public void AccordianTest()
+    {
+        Driver.Navigate().GoToUrl("https://demoqa.com/accordian");
+        var accordianPage = new AccordianPage(Driver);
+
+        int totalButtons = accordianPage.ButtonCount;
+
+        for (int i = 0; i < totalButtons; i++)
+        {
+            accordianPage.ClickButton(i);
+            Assert.True(accordianPage.IsExpanded(i));
+        }
+    }
+}
---
+++ DemoQaTests/Widgets/AccordianPage.cs
@@ -0,0 +1,40 @@
+﻿using DemoQaTests.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoQaTests.Widgets
+{
+    public class AccordianPage : BasePage
+    {
+        public AccordianPage(IWebDriver driver) : base(driver) { }
+
+        private IList<IWebElement> AccordionButtons
+        {
+            get
+            {
+                return Driver.FindElements(By.CssSelector("button.accordion-button"));
+            }
+        }
+
+        public int ButtonCount => AccordionButtons.Count;
+
+        public void ClickButton(int index)
+        {
+            if (AccordionButtons[index].GetAttribute("aria-expanded") == "false")
+            {
+                IWebElement button = AccordionButtons[index];
+
+                ((IJavaScriptExecutor)Driver).ExecuteScript("arguments[0].scrollIntoView(true);", button); //js scroll to elemnt
+                
+                button.Click();
+            }
+        }
+
+        public bool IsExpanded(int index)
+        {
+            return AccordionButtons[index].GetAttribute("aria-expanded") == "true";
+        }
+    }
+}
